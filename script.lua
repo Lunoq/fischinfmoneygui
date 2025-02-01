c6767d87b1258a018ef2e3a9f58f4c8b324e5bda
Lunoq <119677499+Lunoq@users.noreply.github.com>
2025-02-01 11:46:03 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 local http = game.HttpService
-local module = require(game:HttpGet("https://raw.githubusercontent.com/Lunoq/fischinfmoneygui/refs/heads/main/script.lua", true))
+local module = require(game:HttpGet(('https://raw.githubusercontent.com/Lunoq/fischinfmoneygui/refs/heads/main/script.lua')))()
 
 local a = module.MakeWindow("rod")
 
